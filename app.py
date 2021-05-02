afd46dd8170913b0aa28ac7bbdd69954cdcc95b5
sameh mahmoud <sameh.mahmoudgaber@gmail.com>
2021-05-02 20:33:46 +0200

    another-test

--- app.py
+++ app.py
@@ -63,6 +63,8 @@ def predict():
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
+    #logging.basicConfig(filename='/output_txt_files/example.log',level=logging.DEBUG)
+    LOG.info(f"prediction: {prediction}")
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath="199420172020/test"
+kub
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run mypod -it --image=199420172020/test --port=80
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+#kubectl expose pod mypod --type=LoadBalancer --port=8000 --NodePort=8000 --target-port=80
+#kubectl expose deplyoment 
+kubectl port-forward pods/mypod 8000:80
